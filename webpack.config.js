e1f3e76a0fa34a2317031ba306b57af65eaa25af
Tom Lee <user512@users.noreply.github.com>
2019-02-09 11:40:36 -0800

    Babel loader now transpile jsx

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
